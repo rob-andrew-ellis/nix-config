7455fc583b65c2714c79883bf07442529b90c3e9
rob-andrew-ellis <robandrewellis@gmail.com>
2024-09-15 23:26:05 +0100

    49 current  2024-09-15 23:26:04  24.05.4469.6e99f2a27d60  6.6.48                          *

--- packages/hyprland-pkgs.nix
+++ packages/hyprland-pkgs.nix
@@ -21,4 +21,12 @@
   xdg.portal = {
     enable = true;
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      ags = prev.ags.overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ [pkgs.libdbusmenu-gtk3];
+      });
+    })
+  ];
 }
