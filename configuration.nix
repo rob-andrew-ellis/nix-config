ee0d78197275f77af55af1f2e455ed27a29d2cd6
rob-andrew-ellis <robandrewellis@gmail.com>
2024-09-22 14:47:41 +0100

    53 current  2024-09-22 14:47:39  24.05.4469.6e99f2a27d60  6.6.48                          *

--- configuration.nix
+++ configuration.nix
@@ -51,8 +51,13 @@ in {
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # KDE
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland. enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
