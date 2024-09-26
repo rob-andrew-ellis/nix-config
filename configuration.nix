de173367b3898cac9e9912c940d27483d43df4ea
rob-andrew-ellis <robandrewellis@gmail.com>
2024-09-26 22:27:26 +0100

    66 current  2024-09-26 22:27:25  24.05.5266.babc25a577c3  6.6.52                          *

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,8 @@ in {
     ./environment-variables.nix
   ];
 
+  services.hardware.openrgb.enable = true;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
